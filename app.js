9e5787d30b20e882972fa8f43e3bd0b268d7b74f
Manvendra2509 <manvendra.17cse468@rtu.ac.in>
2023-06-04 15:23:34 +0530

    Added failure and success page, added compatibility with Heroku

--- app.js
+++ app.js
@@ -37,11 +37,17 @@ app.post("/", function(req, res) {
 
     const options = {
         method: "post",
-        auth: "manvendra:25cfaa62137048a47683c634c045245d-us10"
-
+        auth: "manvendra:125cfaa62137048a47683c634c045245d-us10"
     }
 
     const request = https.request(url, options, function(response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
         response.on("data", function(data) {
             console.log(JSON.parse(data));
         })
@@ -51,7 +57,11 @@ app.post("/", function(req, res) {
     request.end();
 });
 
-app.listen(3000, function () {
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000.");
 });
 
